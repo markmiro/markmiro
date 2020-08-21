0062e949af6ad124b9b47100030fa38c6f038576
Mark Mironyuk <contact@markmiro.com>
2020-08-21 12:05:17 -0700

    Add scroll to top footer
    This is mostly for mobile users (for now) to get back to top to see the menu

--- components/Page.js
+++ components/Page.js
@@ -5,6 +5,7 @@ import theme from './theme'
 import Nav from './Nav'
 import Spacer from './Spacer'
 import Heading from './Heading'
+import Button from './Button'
 
 const page = css`
   display: flex;
@@ -12,6 +13,7 @@ const page = css`
   @media ${theme.mediaQueryies.mobileAndBelow} {
     flex-direction: column;
   }
+  min-height: 100vh;
 `
 
 const content = css`
@@ -28,26 +30,36 @@ const contentInner = css`
 
 export default function Page({ title, children }) {
   return (
-    <div className={page}>
-      <Head>
-        <title>{title} | Mark Miro</title>
-      </Head>
-      <Nav />
-      <section className={content}>
-        <div className={contentInner}>
-          <Heading
-            style={{
-              fontWeight: theme.fontWeights[2],
-              paddingBottom: 6,
-              borderBottom: '1px solid',
-            }}
-          >
-            {title}
-          </Heading>
-          <Spacer size={4} />
-          {children}
-        </div>
-      </section>
-    </div>
+    <>
+      <div className={page}>
+        <Head>
+          <title>{title} | Mark Miro</title>
+        </Head>
+        <Nav />
+        <section className={content}>
+          <div className={contentInner}>
+            <Heading
+              style={{
+                fontWeight: theme.fontWeights[2],
+                paddingBottom: 6,
+                borderBottom: '1px solid',
+              }}
+            >
+              {title}
+            </Heading>
+            <Spacer size={4} />
+            {children}
+          </div>
+        </section>
+      </div>
+      <footer style={{ padding: theme.space[4] }}>
+        <Button
+          onClick={() => window.scrollTo({ top: 0 })}
+          style={{ width: '100%' }}
+        >
+          Scroll to Top
+        </Button>
+      </footer>
+    </>
   )
 }
