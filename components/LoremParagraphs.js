15a5b6ecf360827c3cae3b6326b14059e51e6709
Mark Mironyuk <contact@markmiro.com>
2020-08-22 20:46:09 -0700

    Tweak LoremParagraphs styles
    Spacing using stack rather than padding

--- components/LoremParagraphs.js
+++ components/LoremParagraphs.js
@@ -2,13 +2,11 @@ import { VStack } from './Stack'
 import theme from './theme'
 
 const LoremParagraphs = ({ count = 3 }) => (
-  <VStack>
+  <VStack space={2}>
     {[...new Array(count)].map((_, i) => (
-      <div
+      <p
         key={i}
         style={{
-          // fontWeight: theme.fontWeights[0],
-          paddingBottom: theme.space[2],
           lineHeight: theme.lineHeights[0],
         }}
       >
@@ -17,7 +15,7 @@ const LoremParagraphs = ({ count = 3 }) => (
         dolor consectetur enim esse occaecat occaecat fugiat mollit ad.
         Adipisicing amet non aliquip Lorem sit consequat ea. Dolor irure esse ad
         ullamco ad esse eiusmod duis nisi minim sunt aliquip.
-      </div>
+      </p>
     ))}
   </VStack>
 )
