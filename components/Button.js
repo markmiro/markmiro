9caae9fae104eaf7cb01b8a5c5c6b192ad46bc72
Mark Mironyuk <contact@markmiro.com>
2020-08-22 20:45:07 -0700

    Fix button border color black in dark mode
    Made color inherit (which is white), so border and text is white now

--- components/Button.js
+++ components/Button.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import theme from './theme'
 
 const Button = styled.button`
+  color: inherit;
   border: 1px solid;
   background: transparent;
   padding: ${theme.space[0]};
