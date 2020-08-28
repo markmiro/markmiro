bf8487abc2079d1e57d6728c59d13f7226577978
Mark Mironyuk <contact@markmiro.com>
2020-08-28 15:53:40 -0700

    Add card component

---
+++ components/Card.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+import theme from './theme'
+
+const Card = styled.div`
+  border: 1px solid ${theme.colors.neutral.light};
+  padding: ${theme.space[2]};
+`
+
+export default Card
