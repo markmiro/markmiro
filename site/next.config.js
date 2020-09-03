405514e6b2bde301bca06ab8329a107fecd59ea1
Mark Mironyuk <contact@markmiro.com>
2020-09-03 11:25:37 -0700

    Add mdx

--- site/next.config.js
+++ site/next.config.js
@@ -7,3 +7,13 @@ module.exports = withReactSvg({
     return config
   },
 })
+
+// From Next.js MDX example
+// https://github.com/vercel/next.js/blob/master/examples/with-mdx/next.config.js
+// const withMDX = require('@next/mdx')({
+//   extension: /\.mdx?$/,
+// })
+
+// module.exports = withMDX({
+//   pageExtensions: ['js', 'jsx', 'mdx'],
+// })
