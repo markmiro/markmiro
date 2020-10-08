64ffd0549422257f146368538e9d8a86a6d2d83a
Mark Mironyuk <contact@markmiro.com>
2020-10-07 20:26:14 -0700

    Add styles to Pre component

--- site/src/components/Json.js
+++ site/src/components/Json.js
@@ -3,7 +3,13 @@ import styled from "@emotion/styled"
 import theme from "./theme"
 
 const Pre = styled.pre`
+  padding: ${theme.space[1]};
+  background-color: ${theme.colors.c1};
+  font-size: ${theme.fontSizes[-1]};
+  font-family: ${theme.fonts.mono};
   line-height: ${theme.lineHeights[0]};
+
+  overflow: scroll;
 `
 const Json = ({ json }) => <Pre>{JSON.stringify(json, null, "  ")}</Pre>
 
