8f6e71e6fd3aa7549cb4b673a4d1071d4431c59e
Mark Miro <markmirony+github@gmail.com>
2022-07-06 22:54:04 -0700

    Add bundle analyzer

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,18 @@
  * @type {import('next').NextConfig}
  */
 
-module.exports = {
-    compiler: {
-        emotion: true,
-    }
-};
+const config = {
+  compiler: {
+    emotion: true,
+  },
+}
+
+if (process.env.ANALYZE === 'true') {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+
+  module.exports = withBundleAnalyzer(config)
+} else {
+  module.exports = config
+}
