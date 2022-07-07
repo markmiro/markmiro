146e9074a8c3ae58ef20465b117d634a97aa9e17
Mark Miro <markmirony+github@gmail.com>
2022-07-06 17:27:19 -0700

    Fix ":first-child" error with VStack and HStack
    Next.js was having trouble with it because of server-side rendering. It wanted ":first-of-type" instead.

--- site/components/Stack.js
+++ site/components/Stack.js
@@ -5,23 +5,12 @@ import theme from './theme'
 export const VStack = styled.div`
   display: flex;
   flex-direction: column;
-
-  & > * {
-    margin-top: ${(props) => theme.space[props.space || 0]};
-  }
-  & > *:first-child {
-    margin-top: 0;
-  }
+  gap: ${(props) => theme.space[props.space || 0]};
 `
 
 export const HStack = styled.div`
   display: flex;
   flex-direction: row;
 
-  & > * {
-    margin-left: ${(props) => theme.space[props.space || 0]};
-  }
-  & > *:first-child {
-    margin-left: 0;
-  }
+  gap: ${(props) => theme.space[props.space || 0]};
 `
