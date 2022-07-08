38db64a0267ffbdcf29618a6e4b928df4f13081d
Mark Miro <markmirony+github@gmail.com>
2022-07-08 12:59:08 -0700

    Move global styles out of _app.js

---
+++ site/components/styles.js
@@ -0,0 +1,51 @@
+import { Global, css } from '@emotion/react'
+import resetCss from 'reset-css/reset.css'
+import theme, { colorsCss, spaceCss } from './theme'
+
+const globalStylesCss = css`
+  ${resetCss}
+  ${colorsCss}
+  ${spaceCss}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  button {
+    margin: 0;
+  }
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+
+    font-size: 16px; /* The base font size everything else is based on */
+    @media ${theme.mediaQueries.mobileSmallAndAbove} {
+      font-size: 18px; /* The base font size everything else is based on */
+    }
+    @media (min-width: 1500px) {
+      font-size: 20px; /* The base font size everything else is based on */
+    }
+    font-family: ${theme.fonts.body};
+    color: ${theme.colors.c8};
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+
+    background: ${theme.colors.c0};
+  }
+
+  hr {
+    margin-top: 0;
+    margin-right: 0;
+    margin-bottom: 0;
+    margin-left: 0;
+  }
+
+  a {
+    color: ${theme.colors.blue};
+    text-decoration: none;
+  }
+`
+
+export const globalStyles = <Global styles={globalStylesCss} />
--- site/pages/_app.js
+++ site/pages/_app.js
@@ -2,67 +2,17 @@
 import React from 'react'
 import NextApp from 'next/app'
 import createCache from '@emotion/cache'
-import { CacheProvider, Global, css } from '@emotion/react'
-import resetCss from 'reset-css/reset.css'
-
-import theme, { colorsCss, spaceCss } from '../components/theme'
+import { CacheProvider } from '@emotion/react'
+import { globalStyles } from '../components/styles'
 
 const cache = createCache({ key: 'next' })
-
-const globalStyles = css`
-  ${colorsCss}
-  ${spaceCss}
-  ${css`
-    ${resetCss}
-  `}
-
-  * {
-    box-sizing: border-box;
-  }
-
-  button {
-    margin: 0;
-  }
-
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-
-    font-size: 16px; /* The base font size everything else is based on */
-    @media ${theme.mediaQueries.mobileSmallAndAbove} {
-      font-size: 18px; /* The base font size everything else is based on */
-    }
-    @media (min-width: 1500px) {
-      font-size: 20px; /* The base font size everything else is based on */
-    }
-    font-family: ${theme.fonts.body};
-    color: ${theme.colors.c8};
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-
-    background: ${theme.colors.c0};
-  }
-
-  hr {
-    margin-top: 0;
-    margin-right: 0;
-    margin-bottom: 0;
-    margin-left: 0;
-  }
-
-  a {
-    color: ${theme.colors.blue};
-    text-decoration: none;
-  }
-`
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
       <CacheProvider value={cache}>
-        <Global styles={globalStyles} />
+        {globalStyles}
         <Component {...pageProps} />
       </CacheProvider>
     )
