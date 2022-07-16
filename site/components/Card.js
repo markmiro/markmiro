752066bf11e6bbe3b9d7c5479647a7abc4aed0fc
Mark Miro <markmirony+github@gmail.com>
2022-07-15 23:33:24 -0700

    Add shadow and hover effect to card component

--- site/components/Card.js
+++ site/components/Card.js
@@ -3,6 +3,15 @@ import theme from './theme'
 
 const Card = styled.div`
   border: 1px solid ${theme.colors.c2};
+  box-shadow: 0 0.1em 3px #00000011;
+
+  transition: border 200ms ease-out;
+  &:hover {
+    border-color: ${theme.colors.c6};
+  }
+`
+
+export const CardBody = styled.div`
   padding: ${theme.space[2]};
 `
 
