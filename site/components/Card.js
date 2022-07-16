b69ecca15959010b12b4a6d7704bbfef5d15c695
Mark Miro <markmirony+github@gmail.com>
2022-07-16 16:43:29 -0700

    Remove card shadow

--- site/components/Card.js
+++ site/components/Card.js
@@ -3,7 +3,6 @@ import theme from './theme'
 
 const Card = styled.div`
   border: 1px solid ${theme.colors.c2};
-  box-shadow: 0 0.1em 3px #00000011;
 
   transition: border 200ms ease-out;
   &:hover {
