200bd8c8e4338d54e41ed235a82343f056bd1311
Mark Miro <contact@markmiro.com>
2022-08-14 12:35:40 -0700

    Fix SSR CSS mismatch
    Remove emotion CacheProvider
    https://emotion.sh/docs/cache-provider
    
    After JS loaded, the CSS was fine, but before that, there were some conflicts. Looks like the SSR CSS didn't have the "next" prefix that was added to emotion styles?

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -1,24 +1,19 @@
 // Stolen from: https://github.com/vercel/next.js/blob/canary/examples/with-emotion-swc/pages/_app.js
 import React from 'react'
 import NextApp from 'next/app'
-import createCache from '@emotion/cache'
-import { CacheProvider } from '@emotion/react'
 import { globalStyles } from '../components/styles'
 // Note: the reset doesn't work with JS disabled in dev, but it does work in prod (according to the docs)
 // https://nextjs.org/docs/basic-features/built-in-css-support#adding-component-level-css
 import 'reset-css/reset.css'
 
-const cache = createCache({ key: 'next' })
-
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
-      // https://emotion.sh/docs/cache-provider
-      <CacheProvider value={cache}>
+      <>
         {globalStyles}
         <Component {...pageProps} />
-      </CacheProvider>
+      </>
     )
   }
 }
